736178c2173a185beb18aef4af1d59807b53cdbc
Mkomarr <m.koomarr@gmail.com>
2019-04-27 16:13:23 +0200

    mobile menu styling

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-/*---------------------- dropdownMenu -------------------------*/
+/*--------- dropdownMenu -----------*/
 
 const icon = document.querySelector('.dropdown-icon');
 const menu = document.querySelector('.top-menu')
 const hideMenu = document.querySelectorAll('.desktopMenu')
 
 icon.addEventListener('click', () => {
-	menu.className = 'top-menu-visible';
+    if (menu.className === 'top-menu') {
+     menu.className = 'top-menu-visible';
+     icon.textContent = "X";
+    } else {
+     menu.className = 'top-menu';
+     icon.innerHTML = `&#9776;`;
+    }
 });
 
 /*------- close menu ----------*/
 
 hideMenu.forEach(singleLi => singleLi.addEventListener('click', () => {
-    menu.className = 'menu';
+    menu.className = 'top-menu';
 }))
