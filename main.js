03308a3ffd395896fba61a431420576d29c0c574
Mkomarr <m.koomarr@gmail.com>
2019-05-07 10:41:13 +0200

    add scrollTo and links in menu

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 
 const icon = document.querySelector('.dropdown-icon');
 const menu = document.querySelector('.top-menu')
-const hideMenu = document.querySelectorAll('.desktopMenu')
+const hideMenu = document.querySelectorAll('.top-menu')
 
 icon.addEventListener('click', () => {
     if (menu.className === 'top-menu') {
-     menu.className = 'top-menu-visible';
+     menu.className = 'top-menu visible';
      icon.textContent = "X";
     } else {
      menu.className = 'top-menu';
@@ -18,4 +18,40 @@ icon.addEventListener('click', () => {
 
 hideMenu.forEach(singleLi => singleLi.addEventListener('click', () => {
     menu.className = 'top-menu';
-}))
+    icon.innerHTML = `&#9776;`;
+}));
+
+
+/*----------- scrolllTo----------------*/
+
+$().ready (function () {
+	let windowWidth = $(window).width();
+
+	$('#kursy-nav').click (function (event){
+		event.preventDefault();
+		let goTop = $('#kursy').offset().top-150;
+		$("html, body").animate({
+			scrollTop: goTop
+		}, 500);
+    });
+
+	$('#kontakt-nav').click (function (event){
+		event.preventDefault();
+		let goTop = $('#kontakt').offset().top-150;
+		$("html, body").animate({
+			scrollTop: goTop
+		}, 500);
+    });
+
+	$('#top-logo').click (function (event){
+		event.preventDefault();
+		let goTop = $('#top').offset().top-150;
+		$("html, body").animate({
+			scrollTop: goTop
+		}, 500);
+    });
+
+
+});
+
+
